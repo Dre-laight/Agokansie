572bf4bd2d8eb8a4e0ba9151c4752c3a2112e9ef
Andre <drelaightbiz@gmail.com>
2026-05-05 14:40:37 +0000

    changed the motivational message

--- src/WelcomeScreen/Welcome.jsx
+++ src/WelcomeScreen/Welcome.jsx
@@ -13,7 +13,7 @@ function WelcomeScreen(){
             <div className='relative flex flex-col justify-center items-center'>
                 <p className='capitalize font-elite text-gold font-bold text-7xl'>Discover Ghanaian <br/> game culture with  </p>
                 <p className='uppercase text-darkgold text-7xl font-bold font-kablammo'>Agokansie</p >
-                <p className='text-xl mt-5 mb-10 text-orange-100 tracking-widest'>Play. Learn. Preserve</p>
+                <p className='text-xl mt-5 mb-10 text-orange-100 tracking-widest'>Learn. Play. Preserve</p>
                 <div className=' flex items-center justify-center'>
                     <button className='border-none p-3 w-60 text-xl text-d rounded-lg animate-bounce cursor-pointer bg-gradient-to-br from-[#A47551] to-[#6B4226] text-[#F7E7CE] uppercase font-bold hover:w-50 transition-smooth duration-300'>Play Now</button>
                 </div>
